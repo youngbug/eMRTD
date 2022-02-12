90f4a13458890c32f19100db8509409de32029d7
Zhao Yang <cnrgc@163.com>
2022-02-12 21:29:08 +0800

    Create file

---
+++ include/emrtd/mrz.h
@@ -0,0 +1,5 @@
+#ifndef __MRZ_H__
+#define __MRZ_H__
+
+int emrtd_mrz_checkdigit(char* str, int len, unsigned char filleroverzero);
+#endif
---
+++ library/mrz.c
@@ -0,0 +1,6 @@
+#include <emrtd/mrz.h>
+
+int emrtd_mrz_checkdigit(char* str, int len, unsigned char filleroverzero)
+{
+  return 0;
+}
